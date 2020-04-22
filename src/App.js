5301e761d622334932e2f4d354d7468b9997f8e6
Matheus <matheusosimone@hotmail.com>
2020-04-22 16:11:13 -0300

    camera app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import CameraPhoto, { FACING_MODES } from 'jslib-html5-camera-photo'
 
-function App() {
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`
+
+const CaptureCore = styled.div`
+  border-radius: 100%;
+  border: 2px solid black;
+  background-color: white;
+  height: 55px;
+  width: 55px;
+  transition: all 0.1s ease;
+`
+
+const CaptureButton = styled.button`
+  height: 70px;
+  width: 70px;
+  border-radius: 100%;
+  background-color: white;
+  position: absolute;
+  bottom: 40px;
+  left: calc(50% - 45px);
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:active,
+  &:hover {
+    & > ${CaptureCore} {
+      border: 5px solid black;
+      height: 45px;
+      width: 45px;
+    }
+  }
+`
+
+const Video = styled.video`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`
+
+const App = () => {
+  const openCamera = async (webcamId) => {
+    const webcam = document.getElementById(webcamId)
+    const camera = new CameraPhoto(webcam)
+    await camera.startCamera(FACING_MODES.USER)
+  }
+
+  useEffect(() => {
+    openCamera('webcam')
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppWrapper>
+      <Video autoPlay playsInline muted id="webcam" />
+      <CaptureButton>
+        <CaptureCore />
+      </CaptureButton>
+    </AppWrapper>
+  )
 }
 
-export default App;
+export default App
